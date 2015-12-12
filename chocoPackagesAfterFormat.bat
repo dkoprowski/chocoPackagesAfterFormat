8b898ceee9a52d57adf7395ae0a2bf1343fe703e
dkoprowski <dkoprowski@outlook.com>
2015-12-12 15:26:24 +0100

    first version of my packages

---
+++ chocoPackagesAfterFormat.bat
@@ -0,0 +1,17 @@
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+choco install -y firefox
+choco install -y git
+choco install -y kdiff3 
+choco install -y nodejs
+choco install -y 7zip
+choco install -y gimp 
+choco install -y winscp.install 
+choco install -y f.lux 
+choco install -y blender
+choco install -y visualstudiocode
+choco install -y slack
+choco install -y bulkrenameutility
+choco install -y clover
+
+shutdown -r -t 300
